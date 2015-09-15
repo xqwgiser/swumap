abfa29336881ec837efc4bcec46fcabafd426e31
XQW <864722821@qq.com>
2015-09-15 18:09:11 +0800

    底图完成

--- app/src/main/java/com/example/xqw/swumap/MainActivity.java
+++ app/src/main/java/com/example/xqw/swumap/MainActivity.java
@@ -1,37 +1,86 @@
 package com.example.xqw.swumap;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
 
+public class MainActivity extends Activity {
+    MapView mMapView = null;                                        //创建地图容器
+    BaiduMap baiduMap=null;                                         //创建百度地图实例
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        mMapView = (MapView) findViewById(R.id.bmapView);
+ //       baiduMap.setMyLocationEnabled(true);
+//        mLocationClient = new LocationClient(getApplicationContext());//声明LocationClient类
+//        mLocationClient.registerLocationListener(myListener);  //注册LocationClient的监听事件
+//        initLocation();
+//        mLocationClient.start();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+        //mLocationClient.stop();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+        //mLocationClient.start();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+        //mLocationClient.stop();
+    }
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span=1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认false，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+    public class MyLocationListener implements BDLocationListener{
+
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+            if(bdLocation==null){
+                return;
+            }
+            MyLocationData locationData=new MyLocationData.Builder().accuracy(bdLocation.getRadius())
+                    .direction(bdLocation.getDerect()).longitude(bdLocation.getLongitude()).build();
+            baiduMap.setMyLocationData(locationData);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
